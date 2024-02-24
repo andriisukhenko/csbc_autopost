a9d53edf82ac3af891b134cb1295ab8ce46818c0
Andrii Sukhenko <andrii.suhenko@gmail.com>
2024-02-24 12:51:05 +0200

    refactored

--- telegram/routers.py
+++ telegram/routers.py
@@ -1,17 +1,17 @@
 from aiogram import Router, types, F
-from app.bot.controllers.news_moderation import news_moderation_controller
-from app.bot.callbacks import NewsModerationCallback
+from telegram.controllers import news_moderation_controller
+from telegram.callbacks import NewsModerationCallback
 
-router = Router()
+moderation_router = Router()
 
-@router.callback_query(NewsModerationCallback.filter(F.action == "accept"))
+@moderation_router.callback_query(NewsModerationCallback.filter(F.action == "accept"))
 async def accept(callback: types.CallbackQuery, callback_data: NewsModerationCallback):
     await news_moderation_controller.publish(callback, callback_data.news_id)
 
-@router.callback_query(NewsModerationCallback.filter(F.action == "decline"))
+@moderation_router.callback_query(NewsModerationCallback.filter(F.action == "decline"))
 async def accept(callback: types.CallbackQuery, callback_data: NewsModerationCallback):
     await news_moderation_controller.decline(callback, callback_data.news_id)
 
-@router.callback_query(NewsModerationCallback.filter(F.action == "regenerate"))
+@moderation_router.callback_query(NewsModerationCallback.filter(F.action == "regenerate"))
 async def accept(callback: types.CallbackQuery, callback_data: NewsModerationCallback):
     await news_moderation_controller.regenarate(callback, callback_data.news_id)
